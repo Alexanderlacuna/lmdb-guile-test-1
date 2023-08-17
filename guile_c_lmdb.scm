52943f5c4f7a566146b5a5e5b5c634dc5dcd0ad2
Alexander_Kabui <alexanderkabua@gmail.com>
2023-08-17 11:05:19 +0300

    make module guile-c-lmdb;rename lmdb-init function

--- guile_c_lmdb.scm
+++ guile_c_lmdb.scm
@@ -1,15 +1,28 @@
-(use-modules (system foreign-library)
-             (ice-9 rdelim)
-             (system foreign)
-  )
-;;refactor code to load this shared library
-(define %lmdb-shared-lib  "./test")
+(define-module (lmdb-guile-test-1 guile-c-lmdb)
+  #:use-module (ice-9 ftw)
+  #:use-module (ice-9 rdelim)
+  #:use-module (system foreign-library)
+  #:use-module (system  foreign)
+  #:export (%lmdb-init-wrapper
+            %lmdb-begin
+            %lmdb-end
+            %lmdb-write
+            %lmdb-read
+            %lmdb-close            
+    ))
 
-(define %scm-mdb-init
+(define %lmdb-shared-lib  "./test")
+(define %lmdb-init
   (foreign-library-function %lmdb-shared-lib "scm_mdb_init"
     #:return-type '* ; Pointer to struct _mdb
     #:arg-types (list '*  int size_t '*)))
 
+;; wrapper for scm_mdb-init
+(define %lmdb-init-wrapper
+    (foreign-library-function %lmdb-shared-lib "init_scm_mdb_wrapper"
+    #:return-type '* ; Pointer to struct _mdb
+    #:arg-types (list '*  int size_t))
+  )
 (define %lmdb-begin (foreign-library-function %lmdb-shared-lib "scm_mdb_begin"
   #:return-type int
   #:arg-types (list '* '* unsigned-int)
